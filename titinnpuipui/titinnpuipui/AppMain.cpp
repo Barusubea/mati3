9e51235dcb5d239f511a38f477d5446c50bf5f47
Barusubea <hayate-makishi-k23h@stu.kbc.ac.jp>
2023-07-20 10:24:23 +0900

    cpp mainの修正
    cpp mainを修正しました

--- titinnpuipui/titinnpuipui/AppMain.cpp
+++ titinnpuipui/titinnpuipui/AppMain.cpp
@@ -1,18 +1,83 @@
 #include"DxLib.h"
+#include"FreamControl.h"
+#include"InputControl.h"
+#include"SceneManager.h"
+
+/*********************************
+* マクロ定義
+**********************************/
+
+#define SCREEN_HEIGHT  (480)  //スクリーンサイズ
+#define SCREEN_WIDTH   (640)  //スクリーンサイズ(幅)
+#define SCREEN_COLOBIT  (32)  //スクリーンカラービット
+#define FONT_SIZE       (20)  //文字サイズ
+
+/**********************************
+* 型定義
+***********************************/
+
+
+
+
+/**********************************
+* グローバル変数定義
+***********************************/
+
+
+
+
+/***********************************
+* プロトタイプ宣言
+************************************/
+
+
+
+/************************************
+* プログラム開始
+*************************************/
 
 int WINAPI WinMain(_In_ HINSTANCE hinstance, _In_opt_ HINSTANCE hPrevinstance, _In_
 	LPSTR ipCmdLine, _In_ int nShowCmd)
 {
+	//ウィンドウタイトル設定
+	SetMainWindowText("Match 3 Puzzle");
+
 	//ウィンドウモードで起動
 	ChangeWindowMode(TRUE);
 
+	//画面サイズの最大サイズ、カラービット数を設定
+	SetGraphMode(SCREEN_WIDTH, SCREEN_HEIGHT,SCREEN_COLORBIT);
+
 	//Dxライブラリの初期化処理
-	if (DxLib_Init() == -1)
+	//エラーが発生したら、終了する
+	if (DxLib_Init() == D_ERROR)
 	{
-		return-1;
+		return D_ERROR;
 
 	}
 
+	//各機能の初期化処理
+	FreamControl_Initialize();  //フレームレート制限機能
+	Input_Initialize();         //入力制御機能
+
+	//シーンマネージャー初期化処理
+	//エラーが発生したら、終了する
+	if(SceneManager_Initialize(E_TITLE)==D_ERROR
+		{
+			return D_ERROR;
+		}
+
+		//描画先画面を裏にする
+		SetDrawScreen(DX_SCREEN_BACK);
+
+		//文字サイズを設定
+		SetFontSize(FONT_SIZE);
+
+		//ゲームループ
+		while (ProcessMessage() != D_ERROR && Input_Escape() == FALSE)
+		{
+			//入力制御機能更新処理
+		}
 	//入力待機
 	WaitKey();
 
