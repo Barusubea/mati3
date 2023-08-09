a990baacf9edbb43c0e935018e53bca3e05740fc
Barusubea <hayate-makishi-k23h@stu.kbc.ac.jp>
2023-08-09 11:30:48 +0900

    k

--- titinnpuipui/titinnpuipui/InputControl.h
+++ titinnpuipui/titinnpuipui/InputControl.h
@@ -1,1 +1,24 @@
 #pragma once
+
+/***********************************
+* マクロ定義
+************************************/
+
+
+/***********************************
+* 型定義
+************************************/
+
+
+/***********************************
+* プロトタイプ宣言
+************************************/
+void Input_Initialize(void);
+void input_UPdate(void);
+int Input_Escape(void);
+
+int GetOldKey(int key);
+int GetNowKey(int key);
+int GetKeyFlg(int kye);
+int GetMousePositionX(void);
+int GetMousePositionY(void);
