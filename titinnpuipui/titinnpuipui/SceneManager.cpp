23f61a531c4c0437e07262c9defb908352c93551
Barusubea <hayate-makishi-k23h@stu.kbc.ac.jp>
2023-08-15 15:08:15 +0900

    stage.cppの追加
    stage.cppを追加しました

--- titinnpuipui/titinnpuipui/SceneManager.cpp
+++ titinnpuipui/titinnpuipui/SceneManager.cpp
@@ -48,5 +48,106 @@ int SceneManager_Initialize(GAME_MODE_mode)
 
 	//ゲームメイン画面
 	Read_Error = GameMainScene_Initialize();
-	if 
+	if (Read_Error == D_ERROR)
+	{
+		return D_ERROR;
+	}
+
+	//ゲームクリア画面
+	Read_Error = GameClearScene_Initialize();
+	if (Read_Error == D_ERROR)
+	{
+		return D_ERROR;
+	}
+
+	//ゲームオーバー画面
+	Read_Error = GameOverScene_Initialize();
+	if (Read_Error == D_ERROR)
+	{
+		return D_ERROR;
+	}
+
+	Game_Mode = mode;
+	Next_Mode = Game_Mode;
+
+	return Read_Error;
+
+}
+
+/***************************************************
+* シーン管理機能：更新処理
+* 引数：なし
+* 戻り値：なし
+****************************************************/
+void SceneManager_Update(void)
+{
+	//前フレームとゲームモードが違っていたらシーンを切り替える
+	if (Game_Mode != Next_Mode)
+	{
+		SceneManager_Initialize(Next_Mode);
+	}
+
+	//各画面の更新処理
+	switch (Game_Mode)
+	{
+	case E_TITLE:
+		TitleScene_Update();
+		break;
+
+	case E_GAMEMAIN:
+		GameMainScene_Update();
+		break;
+
+	case E_GAME_CLEAR:
+		GameClearScene_Update();
+		break;
+
+	case E_GAME_OVER:
+		GameOverScene_Update();
+		break;
+
+	default:
+		break;
+	}
+}
+
+/**************************************************
+* シーン管理機能：描画処理
+* 引数：なし
+* 戻り値：なし
+***************************************************/
+void SceneManager_Draw(void)
+{
+	//各画面の描画処理
+	switch (Game_Mode)
+	{
+	case E_TITLE:
+		TitleScene_Draw();
+		break;
+
+	case E_GAMEMAIN:
+		GameMainScene_Draw();
+		break;
+
+	case E_GAME_CLEAR:
+		GameClearScene_Draw();
+		break;
+
+	case E_GAME_OVER:
+		GameOverScene_Draw();
+		break;
+
+	default:
+		break;
+	}
+}
+
+/*****************************************************
+* シーン管理機能：シーン切り替え処理
+* 引数：変更するゲームモード
+* 戻り値：なし
+******************************************************/
+void Change_Scene(GAME_MODE mode)
+{
+	NEXT_Mode = mode;
 }
